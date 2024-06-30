e4f6dcdee2aa24880b969bee9c58f88c834ef27b
TadekT <tadeusz.toporkiewicz@gmail.com>
2024-06-30 19:19:36 +0200

    dodanie --help , wyswietla dostepne opcje

--- skrypt.sh
+++ skrypt.sh
@@ -13,3 +13,10 @@ if [ "$1" == "--logs" ] && [ -n "$2" ]; then
     echo "File number: $i, Created by: skrypt.sh, Date: $(date)" > log_$i.txt
   done
 fi
+if [ "$1" == "--help" ]; then
+  echo "Available options:"
+  echo "--date        Display the current date"
+  echo "--logs        Create 100 log files"
+  echo "--logs [num]  Create specified number of log files"
+  echo "--help        Display this help message"
+fi
