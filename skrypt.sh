fe56cf8df4e9e0057c3ff879a7ed5e42110c04bc
TadekT <tadeusz.toporkiewicz@gmail.com>
2024-06-30 18:49:02 +0200

    dodanie --logs, tworzenie 100 plików log

--- skrypt.sh
+++ skrypt.sh
@@ -2,3 +2,9 @@
 if [ "$1" == "--date" ]; then
   date
 fi
+if [ "$1" == "--logs" ]; then
+  for i in {1..100}; do
+    echo "File number: $i, Created by: skrypt.sh, Date: $(date)" > log_$i.txt
+  done
+fi
+
