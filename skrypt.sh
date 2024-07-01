ad694e6653f1318e463079e099dd5742694652d1
TadekT <tadeusz.toporkiewicz@gmail.com>
2024-07-01 17:10:52 +0200

    Naprawa logiki kodu

--- skrypt.sh
+++ skrypt.sh
@@ -1,22 +1,23 @@
 #!/bin/bash
+
 if [ "$1" == "--date" ]; then
   date
-fi
-if [ "$1" == "--logs" ]; then
-  for i in {1..100}; do
-    echo "File number: $i, Created by: skrypt.sh, Date: $(date)" > log_$i.txt
-  done
-fi
-
-if [ "$1" == "--logs" ] && [ -n "$2" ]; then
-  for ((i=1; i<=$2; i++)); do
-    echo "File number: $i, Created by: skrypt.sh, Date: $(date)" > log_$i.txt
-  done
-fi
-if [ "$1" == "--help" ]; then
+elif [ "$1" == "--logs" ]; then
+  if [ -n "$2" ]; then
+    for ((i=1; i<=$2; i++)); do
+      echo "File number: $i, Created by: skrypt.sh, Date: $(date)" > log_$i.txt
+    done
+  else
+    for i in {1..100}; do
+      echo "File number: $i, Created by: skrypt.sh, Date: $(date)" > log_$i.txt
+    done
+  fi
+elif [ "$1" == "--help" ]; then
   echo "Available options:"
   echo "--date        Display the current date"
   echo "--logs        Create 100 log files"
   echo "--logs [num]  Create specified number of log files"
   echo "--help        Display this help message"
+else
+  echo "Invalid option. Use --help to see available options."
 fi
